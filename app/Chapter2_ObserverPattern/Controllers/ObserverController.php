30579f82c9d6f1bc164f470307dc965257b98c72
Vanesa Robledo <v.robledo@vehikl.com>
2026-06-11 18:29:13 -0400

    initialize ObserverController

---
+++ app/Chapter2_ObserverPattern/Controllers/ObserverController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Chapter2_ObserverPattern\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ObserverController extends Controller
+{
+    public function index()
+    {
+    }
+}
